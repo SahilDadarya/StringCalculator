20b24ac1f49190c95919cd7cea5def22ff1b9fde
Sahil <sahil.dadarya96@gmail.com>
2020-11-28 22:55:30 +0530

    after refactor

--- src/main/java/com/stringCalculator/StringCalculator/StringCalculator.java
+++ src/main/java/com/stringCalculator/StringCalculator/StringCalculator.java
@@ -1,17 +1,64 @@
 package com.stringCalculator.StringCalculator;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator 
 {
 	 public int add(String text) {
 	       
-		  if (text == null || text.isEmpty()) {
+		 	if (text == null || text.isEmpty()) {
 	            return 0;
 	        }
-		 
-		 return -1;
+	        char customDelimiter = getCustomDelimiter(text.split("\n")[0]);
+
+	        String[] number = (customDelimiter == ',') ? 
+	                                text.split(",|\n") : text.split("\n|" + customDelimiter);
+	        int sum = 0;
+	        List<String> negatives = new ArrayList<String>();
+	        int leaveFirstLine = (customDelimiter == ',') ? 0 : 2;
+	        for (String currentNumber : number) {
+	            if (leaveFirstLine > 0) {
+	                --leaveFirstLine;
+	            } else {
+	                int num = Integer.parseInt(currentNumber);
+	                if (num > 1000) {
+	                    continue;
+	                }
+	                
+	                if (num < 0) {
+	                    negatives.add(currentNumber);
+	                }
+	                sum += num;
+	            }
+	        }
+	        if (!negatives.isEmpty()) {
+	            throw new IllegalArgumentException(
+	                "negatives not allowed " + String.join(",", negatives));
+	        }
+	        return sum;
+	    }
+
+
+	    private char getCustomDelimiter(String line) {
+	        if (line == null || line.isEmpty()) {
+	            return ',';
+	        }
+	        if (convertStrToInt(line)) {
+	            return ',';
+	        }
+	        if (line.length() == 1) {
+	            return line.charAt(0);
+	        }
+	        return ',';
+	    }
+	    
+	    private boolean convertStrToInt(String str) {
+	    	try {
+	    		Integer.parseInt(str);
+	    	} catch (NumberFormatException nfe) {
+	    		return false;
+	    	}
+	    	return true;
 	    }
 }
--- src/test/java/com/stringCalculator/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/stringCalculator/StringCalculator/StringCalculatorTest.java
@@ -1,15 +1,70 @@
 package com.stringCalculator.StringCalculator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-public class StringCalculatorTest 
-{
-	
-    @Test
-    public void emptyStringShouldReturnZero() {
-        StringCalculator sc = new StringCalculator();
-        assertEquals(0, sc.add(""));
-    }
+public class StringCalculatorTest {
+
+	@Test
+	public void returnZeroifEmpty() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(0, sc.add(""));
+	}
+
+	@Test
+	public void returnOneOnly() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(8, sc.add("8"));
+	}
+
+	@Test
+	public void sumOfTwoNumber() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(10, sc.add("3,7"));
+	}
+
+	@Test
+	public void anyNumberDigitSupport() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(110, sc.add("11,99"));
+	}
+
+	@Test
+	public void inputOfNnumber() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(55, sc.add("1,2,3,4,5,6,7,8,9,10"));
+	}
+
+	@Test
+	public void newLineSeparator() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(6, sc.add("1\n2,3"));
+	}
+
+	@Test
+	public void allowCustomDelimiter() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(10, sc.add(";\n1;2;3;4"));
+	}
+
+	@Test
+	public void negativeNotSupported() {
+		StringCalculator sc = new StringCalculator();
+		try {
+			sc.add("-1;4");
+			fail("exception should have been thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("negatives not allowed -1", e.getMessage());
+		}
+	}
+
+	@Test
+	public void numbersGreaterThan1000AreIgnored() {
+		StringCalculator sc = new StringCalculator();
+		assertEquals(sc.add("5,12,1001"), 17);
+		assertEquals(sc.add("14124,22\n4,1214"), 26);
+	}
+
 }
